1538fe7c566e14a6893d71966fe2e6572a1771bd
Anwesh43 <anweshthecool0@gmail.com>
2018-07-10 08:30:02 +0530

    adding OnCompletionListener in Activity

--- app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddectriwaveview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/kotlinlinkeddectriwaveview/MainActivity.kt
@@ -2,12 +2,16 @@ package com.anwesh.uiprojects.kotlinlinkeddectriwaveview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.anwesh.uiprojects.linkeddectriwaveview.LinkedDecTriWaveView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        LinkedDecTriWaveView.create(this, true)
+        val view : LinkedDecTriWaveView = LinkedDecTriWaveView.create(this, true)
+        view.setOnCompletionListener {
+            Toast.makeText(this, "Animation number ${it + 1} completed", Toast.LENGTH_SHORT).show()
+        }
     }
 }
