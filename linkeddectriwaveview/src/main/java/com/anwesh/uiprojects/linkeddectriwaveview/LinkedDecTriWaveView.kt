34396c44f85240d9fb64343c0072d5d32f87e1be
Anwesh43 <anweshthecool0@gmail.com>
2018-07-10 08:17:54 +0530

    adding fixes

--- linkeddectriwaveview/src/main/java/com/anwesh/uiprojects/linkeddectriwaveview/LinkedDecTriWaveView.kt
+++ linkeddectriwaveview/src/main/java/com/anwesh/uiprojects/linkeddectriwaveview/LinkedDecTriWaveView.kt
@@ -113,7 +113,7 @@ class LinkedDecTriWaveView (ctx : Context) : View(ctx) {
         fun draw(canvas : Canvas, paint : Paint) {
             val w : Float = canvas.width.toFloat()
             val h : Float = canvas.height.toFloat()
-            val gap : Float = Math.min(w, h) / 60
+            val gap : Float = w / DTW_NODES
             val sc1 : Float = Math.min(0.5f, state.scale) * 2
             val sc2 : Float = Math.min(0.25f, Math.max(0f, state.scale - 0.5f)) * 4
             val sc3 : Float = Math.min(0.25f, Math.max(0f, state.scale - 0.75f)) * 4
@@ -122,9 +122,9 @@ class LinkedDecTriWaveView (ctx : Context) : View(ctx) {
             paint.color = Color.parseColor("#e74c3c")
             canvas.save()
             canvas.translate(i * gap, h/2)
-            canvas.drawLine(0f, 0f, (gap / 2) * sc1, 0f, paint)
-            canvas.drawLine(gap/2, 0f, gap/2 + (gap/4) * sc2, -gap/4 * sc2, paint)
-            canvas.drawLine(gap / 2 + gap / 4, -gap/4, gap/2 + gap/4 + gap /4 * sc3, -gap/4 + gap/4 * sc3, paint)
+            canvas.drawLine((gap / 2) * sc1, 0f, gap/2, 0f, paint)
+            canvas.drawLine(gap/2 + (gap/4) * sc2, -gap/4 * sc2, gap/2 + (gap/4), -gap/4, paint)
+            canvas.drawLine(gap/2 + gap/4 + gap /4 * sc3, -gap/4 + gap/4 * sc3, gap, 0f, paint)
             canvas.restore()
             next?.draw(canvas, paint)
         }
@@ -157,6 +157,7 @@ class LinkedDecTriWaveView (ctx : Context) : View(ctx) {
                 curr = curr.getNext(dir) {
                     dir *= -1
                 }
+                stopcb(j, scale)
             }
         }
 
